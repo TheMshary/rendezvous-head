96b877d13e6df4a81898a5bff19f5812074f2908
redrip <ska6f8@mail.missouri.edu>
2017-09-25 23:03:53 +0300

    added invitee to the event

--- screens/CreateEvent.js
+++ screens/CreateEvent.js
@@ -8,82 +8,84 @@ import {
   StatusBar
 } from "react-native";
 import { observer } from "mobx-react";
-import store from '../Store';
+import store from "../Store";
 
-const CreateEvent = observer(class CreateEvent extends React.Component {
-  static navigationOptions = {
-    title: "Create Event",
-    headerStyle: {
-      marginTop: StatusBar.currentHeight
+const CreateEvent = observer(
+  class CreateEvent extends React.Component {
+    static navigationOptions = {
+      title: "Create Event",
+      headerStyle: {
+        marginTop: StatusBar.currentHeight
+      }
+    };
+
+    state = {
+      title: "",
+      location: "",
+      time: "",
+      activity: ""
+    };
+
+    handleSubmit() {
+      fetch("http://46.101.75.135/event/", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: "token " + store.token
+        },
+        body: JSON.stringify({
+          title: this.state.title,
+          location: this.state.location,
+          time: this.state.time,
+          activity: this.state.activity
+        })
+      })
+        .then(responseJson => {
+          console.log("RESPONSEJSON.STATUS: " + responseJson.status);
+          responseJson.non_field_errors == null
+            ? alert("Success")
+            : alert(responseJson.non_field_errors);
+        })
+        .catch(error => {
+          console.error(error);
+        });
     }
-  };
 
-  state = {
-    title: "",
-    location: "",
-    time: "",
-    activity: ""
-  };
+    render() {
+      const navigate = this.props.navigation;
+      return (
+        <View>
+          <TextInput
+            placeholder="title"
+            onChangeText={title => this.setState({ title })}
+          />
 
-  handleSubmit() {
-    fetch("http://46.101.75.135/event/", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: "token " + store.token
-      },
-      body: JSON.stringify({
-        title: this.state.title,
-        location: this.state.location,
-        time: this.state.time,
-        activity: this.state.activity
-      })
-    })
-      .then(responseJson => {
-        console.log("RESPONSEJSON.STATUS: " + responseJson.status);
-        message: responseJson.non_field_errors == null
-          ? alert("Success")
-          : alert(responseJson.non_field_errors);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+          <TextInput
+            placeholder="location"
+            onChangeText={location => this.setState({ location })}
+          />
+
+          <TextInput
+            placeholder="time"
+            onChangeText={time => this.setState({ time })}
+          />
+
+          <TextInput
+            placeholder="activity"
+            onChangeText={activity => this.setState({ activity })}
+          />
+
+          <Button
+            onPress={() => {
+              this.handleSubmit();
+            }}
+            title="Create"
+          />
+        </View>
+      );
+    }
   }
-
-  render() {
-    const navigate = this.props.navigation;
-    return (
-      <View>
-        <TextInput
-          placeholder="title"
-          onChangeText={title => this.setState({ title })}
-        />
-
-        <TextInput
-          placeholder="location"
-          onChangeText={location => this.setState({ location })}
-        />
-
-        <TextInput
-          placeholder="time"
-          onChangeText={time => this.setState({ time })}
-        />
-
-        <TextInput
-          placeholder="activity"
-          onChangeText={activity => this.setState({ activity })}
-        />
-
-        <Button
-          onPress={() => {
-            this.handleSubmit();
-          }}
-          title="Create"
-        />
-      </View>
-    );
-  }
-});
+);
 
 export default CreateEvent;
--- screens/Home.js
+++ screens/Home.js
@@ -9,89 +9,126 @@ import {
   ListView
 } from "react-native";
 import { observer } from "mobx-react";
-import store from '../Store';
+import store from "../Store";
 
-const Home = observer(class Home extends React.Component {
-  static navigationOptions = ({ navigation, screenProps }) => ({
-    title: "Events",
-    headerRight: (
-      <Button
-        title="new event"
-        onPress={() => {
-          navigation.navigate("CreateEvent");
-        }}
-      />
-    )
-  });
+const Home = observer(
+  class Home extends React.Component {
+    static navigationOptions = ({ navigation, screenProps }) => ({
+      title: "Events",
+      headerRight: (
+        <Button
+          title="new event"
+          onPress={() => {
+            navigation.navigate("CreateEvent");
+          }}
+        />
+      )
+    });
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      token: this.props.navigation.state.params.token,
-      isLoading: true
+    constructor(props) {
+      super(props);
+      this.state = {
+        token: this.props.navigation.state.params.token,
+        isLoading: true
+      };
+    }
+
+    state = {
+      username: "",
+      message: []
     };
-  }
 
-  componentDidMount() {
-    const token = this.props.navigation.state.params.token;
-    EVENTS_URL = "http://46.101.75.135/event/";
-    return fetch(EVENTS_URL, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: "token " + store.token
-      }
-    })
-      .then(response => response.json())
-      .then(responseJson => {
-        let ds = new ListView.DataSource({
-          rowHasChanged: (r1, r2) => r1 !== r2
-        });
-        // console.log("responseJson: "+responseJson);
-        this.setState(
-          {
-            isLoading: false,
-            dataSource: ds.cloneWithRows(responseJson)
-          },
-          function() {
-            // do something with new state
-          }
-        );
+    onPressInivte = () => {
+      fetch("http://46.101.75.135/event/invite/", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: "token " + store.token
+        },
+        body: JSON.stringify({
+          invitee: this.state.username,
+          pk: 5 //event id
+        })
       })
-      .catch(error => {
-        console.error(error);
-      });
+        .then(response => response.json())
+        .then(responseJson => {
+          responseJson.non_field_errors == null
+            ? alert("Invited")
+            : alert("responseJson.non_field_errors");
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    };
 
-    // return fetch('https://facebook.github.io/react-native/movies.json')
-    //   .then((response) => response.json())
-    //   .then((responseJson) => {
-    //     console.log("responseJson: "+responseJson);
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
-  }
+    componentDidMount() {
+      const token = this.props.navigation.state.params.token;
+      EVENTS_URL = "http://46.101.75.135/event/";
+      return fetch(EVENTS_URL, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: "token " + store.token
+        }
+      })
+        .then(response => response.json())
+        .then(responseJson => {
+          let ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+          });
+          // console.log("responseJson: "+responseJson);
+          this.setState(
+            {
+              isLoading: false,
+              dataSource: ds.cloneWithRows(responseJson)
+            },
+            function() {
+              // do something with new state
+            }
+          );
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
 
-  render() {
-    if (this.state.isLoading) {
+    render() {
+      if (this.state.isLoading) {
+        return (
+          <View style={{ flex: 1, paddingTop: 20 }}>
+            <Text>Loading</Text>
+            <ActivityIndicator />
+          </View>
+        );
+      }
+
       return (
         <View style={{ flex: 1, paddingTop: 20 }}>
-          <Text>Loading</Text>
-          <ActivityIndicator />
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={rowData => <Text>{rowData.title}</Text>}
+          />
+          <View style={{ paddingTop: 20 }}>
+            <TextInput
+              style={{ height: 40 }}
+              placeholder="UserName"
+              onChangeText={UserName => this.setState({ UserName })}
+            />
+
+            <Button
+              onPress={() => {
+                this.onPressInivte();
+              }}
+              title="Invite"
+              color="#841584"
+            />
+          </View>
         </View>
       );
     }
-
-    return (
-      <View style={{ flex: 1, paddingTop: 20 }}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={rowData => <Text>{rowData.title}</Text>}
-        />
-      </View>
-    );
   }
-});
+);
 
 export default Home;
